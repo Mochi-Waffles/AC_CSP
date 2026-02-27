12271e9668047f04907ee4f78a5ec024ee22d5d3
Mochi-Waffles <aven.crawford@ucas-edu.net>
2026-02-27 13:14:28 -0700

    time of day

--- C/Practices/time_of_day.c
+++ C/Practices/time_of_day.c
@@ -9,12 +9,22 @@ int main(){
     printf("What time in millitary time:\n"); 
     scanf("%d", &time);
 
-     if (time <= 11 && time >= 4){
+
+     if (time <= 11 && time >= 1){
         printf("Good morning!\n", time);
-     }else if (time > 11 && time < 2)
-     printf("Good afternoon!\n");
+     
 
+      }else if (time >= 12 && time <= 15){
+         printf("Good afternoon!\n");
 
+     
+      }else if (time >= 16 && time <= 19){
+         printf("Good evening!\n");
+
+              
+      }else if (time >= 20 && time <= 24){
+         printf("Good night!\n");
+      }
 
 return 0;
 }
