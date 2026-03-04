19fac376029bf2aa9c55e034c479b7de7b7f048a
Mochi-Waffles <aven.crawford@ucas-edu.net>
2026-03-04 13:44:27 -0700

    fizz not done

--- C/Notes/arrays.c
+++ C/Notes/arrays.c
@@ -1,6 +1,7 @@
-// AC Lists
+// AC Lists/Arrays
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 
 int main(){
 
@@ -9,6 +10,30 @@ int grades[] = {86, 92, 39, 82, 29};
 printf("%d\n", grades[2]); // single item from array based on index number 
  grades[2] = 23;
 printf("%d\n", grades[2]);
+
+    float distance[]= {72.1, 66.6, 45.2, 27.45};
+
+    char names[][20]={"Mikesa", "Eren", "Vash", "Wolfwood", "Millie", "Nie", "Legato", "Light", "Ryuk"};
+    printf("%s\n", names[5]);
+
+    for(int i = 20; i >= 0; i--){
+        printf("%d\n",  i);
+    }
+
+    int grade_len = sizeof(grades)/sizeof(grades[0]);
+   
+  
+    for(int index = 0; index < grade_len; index++){
+        printf("%d\n", grades[index]+5);
+    }
+
+
+    srand(time(NULL));
+    int goose = rand() % 15 +1;
+    for(int count = 1; count < goose; count++){
+        printf("Duck\n");
+}
+printf("GOOSE\n");
 
     return 0;
 }
---
+++ C/Practices/fizzzbuzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(){
+   
+int start[50]
+
+for(int start = 1; start <= 50; start++){
+   
+
+    if (start/5==0 and start/3==0){
+        printf("FizzBuzz")
+    }else if (start%5==0){
+        printf("Buzz");
+    }else if (start%3==0){
+        printf("Fizz");
+    }else{}
+        print(start);
+    start+=1    
+}}
+    return 0;
+}
