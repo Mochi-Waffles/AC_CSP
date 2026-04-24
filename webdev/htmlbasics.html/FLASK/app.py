af9021a9b567496d5f2c180d318d2d389884a7ec
Mochi-Waffles <aven.crawford@ucas-edu.net>
2026-04-24 13:42:20 -0600

    Buttons

---
+++ webdev/htmlbasics.html/FLASK/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+admin = True
+
+@app.route("/")
+def home():
+   return render_template("index.html", heading="Home Page", content="")
+
+@app.route("/test")
+def testing():
+   return render_template("index.html", heading="test", content="this is nod a drinlsdskfnjfhkjkjkjk HELP HELO ITS MOE RJIDJAS HAEEEELP ")
+
+@app.route("/admin")
+def administrator():
+   if admin:
+      return "You are an administrator"
+   else:
+      return redirect(url_for("home"))
+
+@app.route("/<name>")
+def hello(name):
+   return f"hello {name}"
+
+
+app.run()
